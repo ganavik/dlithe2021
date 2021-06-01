3ceda64f28545118ec2f74b5b4684e8b4e520f21
GANAVI <ganavikumar5699@gmail.com>
2021-06-01 19:20:45 +0530

    simple console

---
+++ Dlithe/src/main/java/ganavi/Dlithe/App.java
@@ -0,0 +1,13 @@
+package ganavi.Dlithe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
