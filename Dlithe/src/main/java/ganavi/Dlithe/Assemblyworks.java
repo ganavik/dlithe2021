17792d2b37adb8b06ad0e255e2bb417a4d384884
GANAVI <ganavikumar5699@gmail.com>
2021-06-04 13:50:25 +0530

    Assemblyworks

--- Dlithe/src/main/java/ganavi/Dlithe/Assemblyworks.java
+++ Dlithe/src/main/java/ganavi/Dlithe/Assemblyworks.java
@@ -1,6 +1,12 @@
 package ganavi.Dlithe.remote;
+import ganavi.Dlithe.model.Assembly;
 
-public interface AssemblyWorks 
-{
+	public interface Assemblyworks 
+	{
 
-}
+		public void createNewRecord(Assembly object);
+		public String toString();
+		public Assembly readOne(Integer number);
+		public String delete(Integer number);
+		public void update(Integer number);
+	}
